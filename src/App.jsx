c96c3d7ac5f97a52be78ac4721fb759e5ad2a34b
João Marcelo Dantas <jmgrd98@gmail.com>
2023-08-15 13:33:18 -0300

    demo and hero components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import {Component} from "react";
+import Demo from "./components/Demo";
+import Hero from "./components/Hero";
+import './App.css';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <main>
+                <div className={'main'}>
+                    <div className={'gradient'}/>
+
+                    <div className={'app'}>
+                        <Hero/>
+                        <Demo/>
+                    </div>
+                </div>
+            </main>
         );
     }
 }
---
+++ src/components/Demo.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Demo() {
+
+        return (
+            <div>
+                
+            </div>
+        );
+}
