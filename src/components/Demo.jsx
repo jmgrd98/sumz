094be39dd6c4a504e3d71d998019e3b41cb1a554
João Marcelo Dantas <jmgrd98@gmail.com>
2023-09-02 10:32:49 -0300

    connecting to api

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,5 +1,6 @@
 import {useState, useEffect} from "react";
 import {copy, linkIcon, loader, tick} from '../assets';
+import {useLazyGetSummaryQuery} from "../services/article.js";
 
 export default function Demo() {
 
@@ -8,18 +9,27 @@ export default function Demo() {
         summary: '',
     });
 
-    async function handleSubmit(e) {
+    const [allArticles, setAllArticles] = useState([]);
 
+    const [getSummary, {error, isFetching}] = useLazyGetSummaryQuery();
+
+    async function handleSubmit(e) {
         e.preventDefault();
 
-        alert('Submitted');
+        const {data} = await getSummary({articleUrl: article.url});
+
+        if(data?.summary) {
+            const newArticle = {...article, summary: data.summary};
+            setArticle(newArticle);
+            setAllArticles([...allArticles, newArticle]);
+            console.log(newArticle);
+        }
     }
 
     return (
         <section className={'mt-16 w-full max-w-xl'}>
             <div className={'flex flex-col w-full gap-2'}>
-                <form className={'relative flex justify-center items-center'} onSubmit={() => {
-                }}>
+                <form className={'relative flex justify-center items-center'} onSubmit={handleSubmit}>
                     <img src={linkIcon} alt={'Link icon'} className={'absolute left-0 my-2 ml-3 w-5'}/>
 
                     <input
@@ -35,7 +45,6 @@ export default function Demo() {
 
                     <button
                         type={'submit'}
-                        onSubmit={handleSubmit}
                         className={'submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'}
                     >
                         Go
--- src/services/article.js
+++ src/services/article.js
@@ -1,11 +1,24 @@
 import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 
+const rapidApiKey = import.meta.env.VITE_RAPID_API_KEY;
 export const articleApi = createApi({
     reducerPath: 'articleApi',
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3000'}),
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', rapidApiKey);
+            headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+            return headers;
+        }
+    }),
     endpoints: (builder) => ({
         getArticles: builder.query({
             query: () => '/articles'
         }),
+        getSummary: builder.query({
+            query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
+        }),
     }),
 });
+
+export const {useGetArticlesQuery, useGetSummaryQuery, useLazyGetSummaryQuery} = articleApi;
