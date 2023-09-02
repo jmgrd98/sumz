144124a356542dea1847b182ce702d98e4fe0c84
João Marcelo Dantas <jmgrd98@gmail.com>
2023-09-02 10:00:28 -0300

    commit

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,23 +1,49 @@
-import { useState, useEffect } from "react";
-import { copy, linkIcon, loader, tick } from '../assets';
+import {useState, useEffect} from "react";
+import {copy, linkIcon, loader, tick} from '../assets';
 
 export default function Demo() {
 
-        return (
-            <section className={'mt-16 w-full max-w-xl'}>
-                <div className={'flex flex-col w-full gap-2'}>
-                    <form className={'relative flex justify-center items-center'} onSubmit={() => {}}>
-                        <img src={linkIcon} alt={'Link icon'} className={'absolute left-0 my-2 ml-3 w-5'}/>
+    const [article, setArticle] = useState({
+        url: '',
+        summary: '',
+    });
 
-                        <input type={'url'} placeholder={'Enter a URL'} value={''} onChange={() => {}} className={'url_input peer'} required/>
+    async function handleSubmit(e) {
 
-                        <button type={'submit'} className={'submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'}>
-                            Go
-                        </button>
-                    </form>
+        e.preventDefault();
+
+        alert('Submitted');
+    }
+
+    return (
+        <section className={'mt-16 w-full max-w-xl'}>
+            <div className={'flex flex-col w-full gap-2'}>
+                <form className={'relative flex justify-center items-center'} onSubmit={() => {
+                }}>
+                    <img src={linkIcon} alt={'Link icon'} className={'absolute left-0 my-2 ml-3 w-5'}/>
+
+                    <input
+                        type={'url'}
+                        placeholder={'Enter a URL'}
+                        value={article.url}
+                        onChange={(e) => {
+                            setArticle({...article, url: e.target.value})
+                        }}
+                        className={'url_input peer'}
+                        required
+                    />
+
+                    <button
+                        type={'submit'}
+                        onSubmit={handleSubmit}
+                        className={'submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700'}
+                    >
+                        Go
+                    </button>
+                </form>
 
 
-                </div>
-            </section>
-        );
+            </div>
+        </section>
+    );
 }
