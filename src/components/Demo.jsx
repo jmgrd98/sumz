d8cbaf6cfbf31a5964595c647428bf8d009d30c8
João Marcelo Dantas <jmgrd98@gmail.com>
2023-09-02 12:55:20 -0300

    Update Demo.jsx

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -76,8 +76,28 @@ export default function Demo() {
             </div>
 
             <div className={'my-10 max-w-full flex justify-center items-center'}>
-                {error && <p className={'text-red-500'}>{error}</p>}
-                {isFetching && <img src={loader} alt={'Loader'} className={'w-20 h-20 object-contain mx-auto'}/>}
+
+                {isFetching ?
+                    <img src={loader} alt={'Loader'} className={'w-20 h-20 object-contain mx-auto'}/>
+                : error ? (<p className={'font-inter font-bold text-black text-center'}>
+                    Well, that wasn't supposed to happen...
+                    <br/>
+                    <span className={'font-satoshi font-normal text-gray-700'}>
+                        {error?.data?.error}
+                    </span>
+                </p>) : (
+                    article.summary && (
+                        <div className={'flex flex-col gap-3'}>
+                            <h2 className={'font-satoshi font-bold text-gray-600 text-xl'}>Article <span className={'blue_gradient'}>Summary</span></h2>
+                            <div className={'summary_box'}>
+                                <p className={'font-inter font-medium text-sm text-gray-700'}>
+                                    {article.summary}
+                                </p>
+                            </div>
+                        </div>
+                    )
+                        )}
+
             </div>
 
         </section>
